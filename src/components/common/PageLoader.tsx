7e36cb17e863bee3aa9b137ff07b128cce96c5f9
lostxmusafir <rajagamer8@gmail.com>
2026-08-02 23:21:15 +0530

    feat: add unique brand page loader animation on branch loder

---
+++ src/components/common/PageLoader.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { getImageUrl } from '../../utils/image';
+import './PageLoader.css';
+
+export function PageLoader() {
+  return (
+    <div className="page-loader-container">
+      <div className="page-loader-content">
+        <div className="loader-logo-wrapper">
+          {/* Base faded logo */}
+          <img 
+            src={getImageUrl('/golden-meraki-logo.png')} 
+            alt="Loading..." 
+            className="loader-logo base-logo" 
+          />
+          {/* Colored logo that wipes in */}
+          <img 
+            src={getImageUrl('/golden-meraki-logo.png')} 
+            alt="Loading..." 
+            className="loader-logo colored-logo" 
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
